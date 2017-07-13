a946277a686b70473867a744a3d39e5f8c3e1c2c
Jang Ei Guk <unagi11@naver.com>
2017-07-13 17:55:55 +0000

    Changed groom and value of A and B

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,12 @@ void func(int&,int&);
 int main(int argc, char* argv[]) {
 	int a, b;
 	func(a, b);
-	cout << "Hello, goorm!" << endl;
+	cout << "Hello, world!" << endl;
 	cout << "a = "<< a << ", b = "<< b << endl;
 	return 0;
 }
 
 void func(int& a, int& b) {
-	a = 20;
-	b = 10;
+	a = 200;
+	b = 100;
 }
