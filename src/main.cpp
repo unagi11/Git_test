8124fbb31d606b8a2ff707a6658941037f220670
Jang Ei Guk <unagi11@naver.com>
2017-07-13 17:02:49 +0000

    add main integer A and B, Builded

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,17 @@
 
 using namespace std;
 
+void func(int&,int&);
+
 int main(int argc, char* argv[]) {
+	int a, b;
+	func(a, b);
 	cout << "Hello, goorm!" << endl;
-	
+	cout << "a = "<< a << ", b = "<< b << endl;
 	return 0;
+}
+
+void func(int& a, int& b) {
+	a = 10;
+	b = 20;
 }
